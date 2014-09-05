acd011626d1cebe6ead4527c768cd4cb48b37c6d
John Haugeland <stonecypher@gmail.com>
2014-09-04 19:07:48 -0700

    first steps

---
+++ controls/flocks-todo.jsx
@@ -0,0 +1,29 @@
+/** @jsx React.DOM */
+/* jshint node: true */
+
+'use strict';
+
+var React  = require('react'),
+    Flocks = require('flocks.js');
+
+/* app shell
+   ---------
+
+   flocks
+   layout
+   md render
+   task list
+   heirarchy
+
+
+
+   outside the control
+   -------------------
+
+   comm stack
+   business logic
+   
+
+*/
+
+module.exports = 2;
