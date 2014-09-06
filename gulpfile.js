de5b7beff1bb6d546dab430db1cd10ea0b47d99e
John Haugeland <stonecypher@gmail.com>
2014-09-06 00:31:04 -0700

    gulp build process

---
+++ gulpfile.js
@@ -0,0 +1,36 @@
+
+// not actually sure how to push these up to the owning project yet
+// putting them in because i wanted to check some stuff and couldn't because it wasn't in the repo
+// but this is not where these will live in the long term
+
+global.errorMessage = '';
+
+var gulp   = require('gulp'),
+    clean  = require('gulp-clean'),
+    react  = require('gulp-react'),
+    yuidoc = require('gulp-yuidoc'),
+    path   = require('path'),
+    sloc   = require('gulp-sloc');
+
+gulp.task('clean', function() {
+  return gulp.src(['./build','./dist'], {read: false}).pipe(clean());
+});
+
+gulp.task('react', ['clean'], function () {
+  return gulp.src('./assets/controls/*.jsx')
+    .pipe(react())
+    .pipe(gulp.dest('./build/react-js'));
+});
+
+gulp.task('yuidoc', ['react'], function() {
+  gulp.src("./assets/js/**/*.js")
+    .pipe(yuidoc())
+    .pipe(gulp.dest("./doc/js"));
+});
+
+gulp.task('sloc', ['yuidoc'], function(){
+  gulp.src(['./build/react-js/*.js','./assets/js/**/*.js'])
+    .pipe(sloc());
+});
+
+gulp.task('default', ['sloc']);
